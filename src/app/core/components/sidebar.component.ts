7d2b9e38856ea4a4f152fac0fd986ef10a1da9ce
Lilly <linhvocaothuy@gmail.com>
2018-05-25 14:44:57 +0700

    Finish sign in/sign out for side bar

--- src/app/core/components/sidebar.component.ts
+++ src/app/core/components/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoreService } from '../services/core.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-sidebar',
@@ -8,17 +9,37 @@ import { CoreService } from '../services/core.service';
 })
 export class SidebarComponent implements OnInit {
     genres: Array<Object> = [];
-    brandName: String = 'Good Movie';
+    brandName: String = 'Good Movies';
     isShowingGenre: boolean;
-    constructor(private _coreService: CoreService) { }
+    isLoggedIn: boolean;
+    user: Object = {};
+
+    constructor(private _coreService: CoreService,
+                private _router: Router) { }
 
     ngOnInit() {
+        // this component is only init-ed once and is not re-inint every route changes
+        // subscribe to this in oder to update isLoggedIn var after login (update name and sign out option)
+        this._router.events.subscribe(event => {
+            this.isLoggedIn = this._coreService.isLogin();
+            if (this.isLoggedIn) {
+                this.user = this._coreService.getCurrentUser();
+            }
+        });
+
         this.isShowingGenre = false;
         this._coreService.getGenres().subscribe(data => this.genres = data);
     }
 
     onMenuClicked() {
         this.isShowingGenre = !this.isShowingGenre;
+    }
+
+    logOut(): void {
+        this._coreService.logout();
+        this.isLoggedIn = false;
+        this.user = {};
+        this._router.navigate(['']);
     }
 
 }
